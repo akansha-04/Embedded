5815d77ae3bff753f18b6e471e712c6845a70762
akansha-04 <102888432+akansha-04@users.noreply.github.com>
2024-03-16 22:49:43 +0530

    Update app_embedded.py

--- app_embedded.py
+++ app_embedded.py
@@ -9,7 +9,7 @@ from my_model.model import FacialExpressionModel
 # Set up the necessary objects and parameters
 st.set_option('deprecation.showfileUploaderEncoding', False)
 face_cascade = cv2.CascadeClassifier("frecog/haarcascade_frontalface_default.xml")
-model = FacialExpressionModel("my_model/model.json", "my_model/model_weights.h5")
+model = FacialExpressionModel("my_model/model.json/", "my_model/model_weights.h5/")
 font = cv2.FONT_HERSHEY_SIMPLEX
 
 # Define the function to detect faces
